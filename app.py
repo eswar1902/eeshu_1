8a5e5d5a664e7c3b76ed620278f04d8f298ee725
eswar1902 <104787648+eswar1902@users.noreply.github.com>
2024-01-03 12:37:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request
+from args import *
+import numpy as np
+import pickle
+with open('Model.pkl','rb') as mod:
+    model=pickle.load(mod)
+with open('Scaler.pkl','rb') as mod:
+    scaler=pickle.load(mod)    
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def index():
+    #return 'I am in first page'
+    #print(request.method)
+    #print(request.form)
+    if request.method=='POST':
+        bedrooms=request.form['bedrooms']
+        bathrooms=request.form['bathrooms']
+        sft=request.form['sft']
+        location=request.form['location']
+        status=request.form['status']
+        property_type=request.form['property_type']
+        direction=request.form['direction']
+        input_array=np.array([[bedrooms,
+                              bathrooms,
+                              location,sft,status,
+                              direction,property_type]])
+        t_array=scaler.transform(input_array)
+        prediction=model.predict(t_array)[0]
+        return render_template('index.html',location_mapping=location_mapping,status_mapping=status_mapping,
+                               direction_mapping=direction_mapping,property_type_mapping=property_type_mapping,
+                               prediction=prediction)
+        
+        
+    else:
+        return render_template('index.html',location_mapping=location_mapping,status_mapping=status_mapping,
+                               direction_mapping=direction_mapping,
+                               property_type_mapping=property_type_mapping)
+@app.route('/second')
+def second():
+    return 'I am in second page'
+@app.route('/third')
+def third():
+    return ' I am third page'
+if __name__=='__main__':
+#app.run(use_reloader=True,debug=True)
+    app.run()
